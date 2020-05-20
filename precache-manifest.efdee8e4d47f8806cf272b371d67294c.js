c3e0766cf1caf819d2dee99eb40f5746080f8cb5
vaibhav <it@vertipridesolution.com>
2020-05-20 10:14:28 +0530

    Updates

---
+++ precache-manifest.efdee8e4d47f8806cf272b371d67294c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc52b23e903e5658d66f1e3463cbcfd",
+    "url": "/reactapp2/index.html"
+  },
+  {
+    "revision": "0c8215c1541d145a9597",
+    "url": "/reactapp2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad192a4f93625d5ed4b6",
+    "url": "/reactapp2/static/js/2.7d89bd52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapp2/static/js/2.7d89bd52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c8215c1541d145a9597",
+    "url": "/reactapp2/static/js/main.b2e54684.chunk.js"
+  },
+  {
+    "revision": "c3650c05f8d7cf278b22",
+    "url": "/reactapp2/static/js/runtime-main.9cde69d0.js"
+  }
+]);
